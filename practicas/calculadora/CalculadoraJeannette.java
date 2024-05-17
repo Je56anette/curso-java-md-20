0cf11eddbe5e9c7c9fbf3c50d56085f28bcd9643
Jeannette Fuentes Piña <163625380+Je56anette@users.noreply.github.com>
2024-05-17 02:31:06 -0600

    CalculadoraJeannette.java
    Práctica de creación de una calculadora en java

---
+++ practicas/calculadora/CalculadoraJeannette.java
@@ -0,0 +1,26 @@
+//public package java.practicas.calculadora;
+
+class CalculadoraJeannette {
+public static void main(String[] args) {
+    //Variables a calcular
+    int valor1= 20;
+    int valor2= 30;
+
+    //Suma
+    int suma= valor1+valor2;
+    System.out.println("El resultado de mi suma es "+suma);
+
+    //Resta
+    int resta= valor1-valor2;
+    System.out.println("El resultado de mi resta es "+resta);
+
+    //Multiplicación
+    int multiplicación= valor1*valor2;
+    System.out.println("El resultado de la multiplicación es "+multiplicación);
+
+    //División
+    int división= valor1/valor2;
+    System.out.println("El resultado de la división es "+división);
+}
+    
+}
